bf1b1cf77c1f775cbc1135f10fe3e5082fa70340
Daniel Chesters <daniel.chesters@gmail.com>
2018-03-26 21:23:35 +0200

    :arrow_up: Migrate to Junit 5

--- src/test/kotlin/fr/danielchesters/eventLocator/models/EventTests.kt
+++ src/test/kotlin/fr/danielchesters/eventLocator/models/EventTests.kt
@@ -1,19 +1,19 @@
 package fr.danielchesters.eventLocator.models
 
 import fr.danielchesters.eventLocator.repositories.EventRepository
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.LocalDateTime
 
 /**
  * @author Daniel Chesters (on 25/03/2018).
  */
-@RunWith(SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
 class EventTests {
     @Autowired
@@ -31,7 +31,7 @@ class EventTests {
         assertEquals(event.name, eventFromDb.name)
         assertEquals(event.date, eventFromDb.date)
         assertEquals(event.description, eventFromDb.description)
-        assertEquals(event.latitude, eventFromDb.latitude, 0.0)
-        assertEquals(event.longitude, eventFromDb.longitude, 0.0)
+        assertEquals(event.latitude, eventFromDb.latitude, 0.01)
+        assertEquals(event.longitude, eventFromDb.longitude, 0.01)
     }
 }
